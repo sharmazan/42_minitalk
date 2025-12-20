6c324488ed0d42aedf45d697609cf40cd8ae6b09
Sergii Sharmazanov <ssharmaz@c2r2p8.42vienna.com>
2025-12-20 18:17:27 +0100

    WiP

--- src/client/client.c
+++ src/client/client.c
@@ -6,19 +6,59 @@
 /*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 23:17:00 by ssharmaz          #+#    #+#             */
-/*   Updated: 2025/12/20 16:59:42 by ssharmaz         ###   ########.fr       */
+/*   Updated: 2025/12/20 18:15:42 by ssharmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf/libftprintf.h"
 #include <unistd.h>
 
-int	main(int ac, char **av)
+// int	main(int ac, char **av)
+// {
+// 	if (ac != 3)
+// 	{
+// 		ft_printf("Usage:\n%s <SERVER_PID> <MESSAGE>!!!\n", av[0]);
+// 		return (0);
+// 	}
+// 	ft_printf("My PID is %d\n", getpid());
+// }
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
 {
-	if (ac != 3)
+	pid_t	pid;
+	char	*message;
+	int		sig;
+
+	// int		which;
+	if (argc != 3)
 	{
-		ft_printf("Usage:\n%s <SERVER_PID> <MESSAGE>!!!\n", av[0]);
-		return (0);
+		fprintf(stderr, "Usage: %s <PID> <1|2>\n", argv[0]);
+		return (1);
 	}
-	ft_printf("My PID is %d\n", getpid());
+	pid = (pid_t)atoi(argv[1]);
+	message = argv[2];
+	fprintf(stderr, "Message: %s\n", message);
+	while (*message != 0)
+	{
+		fprintf(stderr, "%c\n", *message);
+		// which = atoi(av[2]);
+		if (*message == '1')
+			sig = SIGUSR1;
+		else
+			sig = SIGUSR2;
+		// sig = ((char)*message == '1') ? SIGUSR1 : SIGUSR2;
+		if (kill(pid, sig) == -1)
+		{
+			perror("kill");
+			return (1);
+		}
+		message += 1;
+		sleep(1);
+	}
+	return (0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -6,14 +6,60 @@
 /*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 23:17:00 by ssharmaz          #+#    #+#             */
-/*   Updated: 2025/12/20 16:25:01 by ssharmaz         ###   ########.fr       */
+/*   Updated: 2025/12/20 17:39:48 by ssharmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf/libftprintf.h"
 #include <unistd.h>
 
+// int	main(void)
+// {
+// 	ft_printf("%d\n", getpid());
+// }
+
+#define _POSIX_C_SOURCE 200809L
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static void	on_signal(int sig)
+{
+	const char	*msg = NULL;
+
+	if (sig == SIGUSR1)
+		msg = "Caught SIGUSR1\n";
+	else if (sig == SIGUSR2)
+		msg = "Caught SIGUSR2\n";
+	else
+		msg = "Caught other signal\n";
+	// async-signal-safe output:
+	(void)write(STDOUT_FILENO, msg, strlen(msg));
+}
+
 int	main(void)
 {
-	ft_printf("%d\n", getpid());
+	struct sigaction	sa;
+
+	printf("PID: %ld\n", (long)getpid());
+	fflush(stdout);
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = on_signal;
+	// Optional: restart some interrupted syscalls automatically
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask); // signals blocked while handler runs
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		perror("sigaction SIGUSR1");
+		return (1);
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction SIGUSR2");
+		return (1);
+	}
+	// Wait for signals forever
+	for (;;)
+		pause();
 }
