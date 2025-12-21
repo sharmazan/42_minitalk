44c5121bdf4982529c7c2e847f8b93ae9e08fee4
Sergii Sharmazanov <ssharmaz@c2r2p8.42vienna.com>
2025-12-21 17:39:52 +0100

    Recompile client on ft_header change

--- src/inc/ft_header.h
+++ src/inc/ft_header.h
@@ -6,14 +6,14 @@
 /*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 23:17:00 by ssharmaz          #+#    #+#             */
-/*   Updated: 2025/12/21 17:15:28 by ssharmaz         ###   ########.fr       */
+/*   Updated: 2025/12/21 17:38:50 by ssharmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_HEADER_H
 # define FT_HEADER_H
 
-#define STDOUT 1
-#define STDERR 2
+# define STDOUT 1
+# define STDERR 2
 
 #endif
